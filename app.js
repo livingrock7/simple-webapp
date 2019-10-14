30c70e5ff61d9bdee65464ceaf63da454fb18c87
livingrock7 <chiragtitiya6893@gmail.com>
2019-10-14 23:59:29 +0530

    web3

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var express = require("express");
+const express = require('express');
 var path = require("path");
 const bodyParser = require('body-parser');
-
+//const request = require('request');
 var routes = require("./routes");
 
 var app = express();
 
-app.set("port", process.env.PORT || 3000);
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs"); //ejs is templating 
+app.set("port", process.env.PORT || 3000);
 
 //app.use(routes);
 
@@ -21,11 +22,9 @@ app.get("/", function(req,res){
 
 app.post("/", function(req,res){
    console.log(req.body.recipient);
-   res.render("index");
+   //res.render("index");
 });
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(app.get("port"), function(){
    console.log("server started on port " + app.get("port")); 
